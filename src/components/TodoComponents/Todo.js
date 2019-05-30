ae8c8cb397fba7a5188d1d466d38ec0b92470b40
unknown <robertrey1234@gmail.com>
2019-05-30 13:17:13 -0500

    Slowly adding to MVP. List has been made and form is in progress.

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Todo(props) {
+    return (
+        <div>
+            <p>{props.task.task}</p>
+        </div>
+    )
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
 // your components will all go in this `component` directory.
+import React from 'react'
+import Todo from "./Todo"
 
+function TodoList(props) {
+    return (
+        <div>
+            {props.tasks.map((task) => <Todo task={task}/>)}
+        </div>
+    )
+}
+
+export default TodoList
